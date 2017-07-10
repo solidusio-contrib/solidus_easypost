1294724e7d70b053ed1e76dfcb1ebbcc23e18183
hf2186 <houtan@sideprojects.com>
2017-07-10 16:08:46 -0400

    Adding install file for migrations

---
+++ lib/generators/solidus_easypost/install/install_generator.rb
@@ -0,0 +1,23 @@
+module SolidusEasypost
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      class_option :auto_run_migrations, type: :boolean, default: false
+
+      argument :file_name, type: :string, desc: 'rails app_path', default: '.'
+      source_root File.expand_path('../../templates', __FILE__)
+
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=solidus_easypost'
+      end
+
+      def run_migrations
+        run_migration = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask 'Would you like to run the migrations now? [Y/n]')
+        if run_migration
+          run 'bundle exec rake db:migrate'
+        else
+          puts 'Skipping rake db:migrate, don\'t forget to run it!'
+        end
+      end
+    end
+  end
+end
