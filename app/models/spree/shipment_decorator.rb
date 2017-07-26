e2fb71a5c0d1f08608175133ce05adaaf52f02d7
Houtan <hf2186@users.noreply.github.com>
2017-07-26 11:29:36 -0400

    Adding Check before buying

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -39,7 +39,7 @@ module Spree
         rate.id == selected_easy_post_rate_id
       end
 
-      easypost_shipment.buy(rate)
+      easypost_shipment.buy(rate) unless easypost_shipment.postage_label
       self.tracking = easypost_shipment.tracking_code
     end
   end
