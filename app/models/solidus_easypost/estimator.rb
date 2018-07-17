6a82cf72129d07003b9dd40f57ab9ab549ff0473
Piotr Biegaj <PeteTheSadPanda@users.noreply.github.com>
2018-07-16 20:09:28 -0500

    Create Spree::Store association to newly created Spree::ShippingMethod when creating via a find_or_create inside of SolidusEasypost::Estimator, also ensure a unique Spree::Store shipping_method association via a unique index

--- app/models/solidus_easypost/estimator.rb
+++ app/models/solidus_easypost/estimator.rb
@@ -2,6 +2,7 @@ module SolidusEasypost
   class Estimator
     def shipping_rates(package, frontend_only = true)
       shipment = package.easypost_shipment
+      store = package.order.store
       rates = shipment.rates.sort_by { |r| r.rate.to_i }
 
       shipping_rates = []
@@ -13,7 +14,7 @@ module SolidusEasypost
             cost: rate.rate,
             easy_post_shipment_id: rate.shipment_id,
             easy_post_rate_id: rate.id,
-            shipping_method: find_or_create_shipping_method(rate)
+            shipping_method: find_or_create_shipping_method(rate, store)
           )
 
           shipping_rates << spree_rate if spree_rate.shipping_method.frontend?
@@ -35,15 +36,23 @@ module SolidusEasypost
     # Cartons require shipping methods to be present, This will lookup a
     # Shipping method based on the admin(internal)_name. This is not user facing
     # and should not be changed in the admin.
-    def find_or_create_shipping_method(rate)
+    def find_or_create_shipping_method(rate, store)
       method_name = "#{ rate.carrier } #{ rate.service }"
-      Spree::ShippingMethod.find_or_create_by(admin_name: method_name) do |r|
+      sm = Spree::ShippingMethod.find_or_create_by(admin_name: method_name) do |r|
         r.name = method_name
         r.display_on = 'back_end'
         r.code = rate.service
         r.calculator = Spree::Calculator::Shipping::FlatRate.create
         r.shipping_categories = [Spree::ShippingCategory.first]
       end
+
+      begin
+        store.shipping_methods << sm
+      rescue ActiveRecord::RecordNotUnique => e
+        # do nothing, this is to prevent duplicates
+      end
+
+      sm
     end
   end
 end
