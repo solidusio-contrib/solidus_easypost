962ba68912ecf5911807b9c7a782bd299169d52d
Allison Reilly <acreilly3@gmail.com>
2020-03-14 14:46:02 -0300

    Add explanation of eel_pfc method

--- app/models/solidus_easypost/customs_info.rb
+++ app/models/solidus_easypost/customs_info.rb
@@ -27,6 +27,14 @@ module SolidusEasypost
 
     private
 
+    # When shipping outside the US, you need to provide either an Exemption and Exclusion Legend (EEL) code
+    # or a Proof of Filing Citation (PFC). Which you need is based on the value of the goods being shipped.
+
+    # - If the value of the goods is less than $2,500, then you pass the following EEL code: "NOEEI 30.37(a)"
+    # - If the value of the goods is greater than $2,500, you need to get an Automated Export System (AES) Internal Transaction Number (ITN) for your shipment. ITN will look like “AES X20120502123456”. To get an ITN, go to the AESDirect website.
+
+    # An ITN is required for any international shipment valued over $2,500 and/or requires an export license unless exemptions apply.
+
     def eel_pfc
       if order.total < 2500
         'NOEEI 30.37(a)'
