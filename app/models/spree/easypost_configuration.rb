17b5a2ba8fcadab39a66986a8dffe5aa71d9f399
Allison Reilly <acreilly3@gmail.com>
2020-03-25 16:50:24 -0300

    Add ability to disable API

--- app/models/spree/easypost_configuration.rb
+++ app/models/spree/easypost_configuration.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
 class Spree::EasypostConfiguration < Spree::Preferences::Configuration
+  preference :api_enabled, :boolean, default: true
   preference :ddp_enabled, :boolean, default: false
 end
