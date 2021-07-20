8cb52de4bff88c0758a7808a8e07b190f6efa07b
vassalloandrea <andrea.vassallo.94@gmail.com>
2021-07-20 22:15:52 +0200

    Allow to create a parcel passing other properties
    This is needed to send more information to
    Easypost which will returns the rates
    with more accurate prices.
    
    Check the easypost gem documentation
    for more information about the Parcel
    object: https://github.com/EasyPost/easypost-ruby#example

--- lib/solidus_easypost/parcel_builder.rb
+++ lib/solidus_easypost/parcel_builder.rb
@@ -4,16 +4,43 @@ module SolidusEasypost
   class ParcelBuilder
     class << self
       def from_package(package)
-        total_weight = package.contents.sum do |item|
-          item.quantity * item.variant.weight
-        end
+        params = {
+          weight: package_property_total(property: :weight, package: package)
+        }
 
-        ::EasyPost::Parcel.create weight: total_weight
+        total_width = package_property_total(property: :width, package: package)
+        total_height = package_property_total(property: :height, package: package)
+        total_depth = package_property_total(property: :depth, package: package)
+
+        params[:width] = total_width unless total_width.zero?
+        params[:height] = total_height unless total_height.zero?
+        params[:length] = total_depth unless total_depth.zero?
+
+        ::EasyPost::Parcel.create(**params)
       end
 
       def from_return_authorization(return_authorization)
-        total_weight = return_authorization.inventory_units.joins(:variant).sum(:weight)
-        ::EasyPost::Parcel.create weight: total_weight
+        params = {
+          weight: return_authorization.inventory_units.joins(:variant).sum(:weight)
+        }
+
+        total_width = return_authorization.inventory_units.joins(:variant).sum(:width)
+        total_height = return_authorization.inventory_units.joins(:variant).sum(:height)
+        total_depth = return_authorization.inventory_units.joins(:variant).sum(:depth)
+
+        params[:width] = total_width unless total_width.zero?
+        params[:height] = total_height unless total_height.zero?
+        params[:length] = total_depth unless total_depth.zero?
+
+        ::EasyPost::Parcel.create(**params)
+      end
+
+      private
+
+      def package_property_total(property:, package:)
+        package.contents.sum do |item|
+          item.quantity * (item.variant.public_send(property) || 0)
+        end
       end
     end
   end
